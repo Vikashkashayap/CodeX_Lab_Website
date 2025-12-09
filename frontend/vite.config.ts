5da1648e30f01da6dd10d2ad357d2119083e0a5d
Vikashkashayap <vikashkashyap756@gmail.com>
2025-12-09 23:04:06 +0530

    Resolve the issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './frontend',
+  root: '.',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
 
